fd2404517f9b84afea0c87f501c1511615f2403c
Mitchell Perry <mitchell.perry@simplybusiness.co.uk>
2022-05-13 10:33:11 +0100

    first commit

---
+++ complex_numbers_spec.rb
@@ -0,0 +1,13 @@
+describe 'adding complex numbers' do
+  example '0 + 0'
+  example '0 + 1'
+  example '1 + 0'
+  example '1 + 1'
+  example '1 + 2'
+  example '2 + 1'
+  example '2 + 3i + 3 + 4i'
+  example '3 + 4i + 2 + 3i'
+  example '3 + 4i + 10'
+  example '5 + 10i + 2i'
+  example '3 + i + 0.5'
+end
